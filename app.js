5e212aca8d4fcc4dbc1929a7be6d7230b426fc9b
unknown <kongpakpiwphong1@gmail.com>
2023-09-27 13:57:11 +0700

    changes port

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.use('/user',userRouter);
 const uri = "mongodb://localhost:27017/project";
 
 mongoose.connect(uri)
-    .then((result)=> app.listen(3000, () => {
-        console.log('App is running on port 3000 http://localhost:3000/');
+    .then((result)=> app.listen(4000, () => {
+        console.log('App is running on port 4000 http://localhost:3000/');
     }))
     .catch((err) => console.log(err))
 app.use(function(req, res, next) {
