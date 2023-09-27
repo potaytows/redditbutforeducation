2675c87261a0fce6293d61d91c508b3021b509a7
potaytows <chiza11233@gmail.com>
2023-09-28 03:07:38 +0700

    Adeed server-side Used-Email validation

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -10,20 +10,16 @@ const page_index = (req, res,) => {
 const page_login = (req, res,) => {
   res.render('login', { pageTitle: 'Reddeetznuts : Login' });
 
+
+
 };
 
 const page_register = (req, res,) => {
-  Users.find({},{'email':1,'_id':0})
-  .then((result)=>{
-    res.render('register', { pageTitle: 'Reddeetznuts : Register',usedEmail : result });
-    
-}).catch((err)=>{
-  console.log(err)
-})
+  res.render('register', { pageTitle: 'Reddeetznuts : Register' });
 };
-const logout = (req,res)=>{
+const logout = (req, res) => {
   req.session.destroy();
-  res.cookie('isLoggedout','true',{maxAge:1000})
+  res.cookie('isLoggedout', 'true', { maxAge: 1000 })
   res.redirect('/');
 };
 
