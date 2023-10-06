11c57feb1b756ab145a5235341e45f74d23cd4e5
unknown <kongpakpiwphong1@gmail.com>
2023-10-07 01:21:02 +0700

    new

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 
 const UserSchema = new mongoose.Schema({
+    
     email:{
         type:String,
         required:true,
         unique:true
+    },
+    username:{
+        type:String,
+        required:true,
     },
     password:{
         type:String,
